83ff159bbce029156462afeca5d744471b221826
ehbong <hangillw86@gmail.com>
2020-05-05 20:17:21 +0900

    함수 추가 버튼 추가

--- regFunction.js
+++ regFunction.js
@@ -1,5 +1,12 @@
 let regFunc = {
-    ctBracketTxt : function (bracketType, inText){
+    regHistory : []
+    , pushHistory : function(regtext){
+        this.regHistory.push(this.regHistory);
+    }
+    , backHistory : function(){
+        return this.regHistory.pop();
+    }
+    , ctBracketTxt : function (bracketType, inText){
         let bracketTypeOpen;
         let bracketTypeClose;
         if(bracketType == '[' || bracketType == ']' || bracketType == '[]'){
@@ -16,9 +23,24 @@ let regFunc = {
         }
         return bracketTypeOpen + inText + bracketTypeClose;
     }
+    , plusOption : function(mark, num, min, max){
+        if(arguments.length > 1){
+            if(typeof min == "number"){
+                return "{"+min+",}";
+            }
+            if(typeof max == "number" || typeof min == "number"){
+                return "{"+min+","+max+"}";
+            }
+            if(typeof num == "number"){
+                return "{"+num+"}";
+            }
+        }else{
+            return mark;
+        }
+    }
     , createRegExp : function(regText){
-        return new RegExp('()'+regText+'()');
-
+        this.pushHistory(regText);
+        return new RegExp(regText);
     }
 }
 
